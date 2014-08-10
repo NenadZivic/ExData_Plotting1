961e5a18be1cce7e0754ef2901be01de35e874e4
NenadZivic <nenadz@Nordeuss-MacBook-Pro.local>
2014-08-10 22:24:57 +0200

    Coursera Data Science
    Plots 1 and 2 for the course.

---
+++ plot1.R
@@ -0,0 +1,7 @@
+power_con <- read.table("household_power_consumption.txt", sep = ";", header = TRUE);
+power_con_feb12 <- power_con[66638:(66638 + 1440 + 1440),];
+list_gl_ac_pow = power_con_feb12$Global_active_power;
+list_num <- as.numeric(list_gl_ac_pow) / 500;
+hist(list_num, col = "red", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, "plot1.png");
+dev.off();
